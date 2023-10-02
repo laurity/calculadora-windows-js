8d5da0b4e601f5ac878ce1a3ea9ac9f6b564ac65
Laura López <99351746+laurity@users.noreply.github.com>
2023-10-02 19:38:16 +0000

    botones de numeros

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,44 @@
 //Para que funcione, necesita que carge todo el archivo HTML y CSS
-document.addEventListener("DOMContentLoaded", () =>{
-    let display = document.getElementById("display");
-    let saveInput = "0";
-    let currentInput = "0";
+document.addEventListener("DOMContentLoaded", () => {
+
+  const displayTop = document.getElementById('save-operation');
+  const displayBottom = document.getElementById('first-valor');
+  const numbers = [
+    document.getElementById("num0"),
+    document.getElementById("num1"),
+    document.getElementById("num2"),
+    document.getElementById("num3"),
+    document.getElementById("num4"),
+    document.getElementById("num5"),
+    document.getElementById("num6"),
+    document.getElementById("num7"),
+    document.getElementById("num8"),
+    document.getElementById("num9"),
+    document.getElementById("pi"),
+    document.getElementById(","),
+  ];
+  const operators = [
+    document.getElementById("percentage"),
+    document.getElementById("fraction"),
+    document.getElementById("square"),
+    document.getElementById("square-root"),
+    document.getElementById("divide"),
+    document.getElementById("multiply"),
+    document.getElementById("subtract"),
+    document.getElementById("add"),
+    document.getElementById("toggle-sign"),
+  ];
+
+  const equal = document.getElementById("equals");
+  const numberInput = (e) => {
+    let number = parseFloat(e.target.value);
+    if (e.target.id === "pi") {
+      number = Math.PI.toFixed(8);
+    }
+    displayBottom.innerText = number;
+  }
+  numbers.forEach(n => {
+    n.addEventListener('click', numberInput);
+  });
 
 })
