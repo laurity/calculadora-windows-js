72c53b353115c4171afade52c8bbe2d115746290
Laura López <lauralopez166@gmail.com>
2023-10-02 12:19:52 +0200

    falta js

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+//Para que funcione, necesita que carge todo el archivo HTML y CSS
+document.addEventListener("DOMContentLoaded", () =>{
+    let display = document.getElementById("display");
+    let saveInput = "0";
+    let currentInput = "0";
+
+})
