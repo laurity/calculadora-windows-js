8d30a28ab243ba19dbffc9fa5d669f59b47bce56
Laura López <99351746+laurity@users.noreply.github.com>
2023-10-03 20:32:07 +0000

    añadidos los operadores

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("num8"),
     document.getElementById("num9"),
     document.getElementById("pi"),
-    document.getElementById(","),
+    document.getElementById("decimal"),
   ];
   const operators = [
     document.getElementById("percentage"),
@@ -33,12 +33,71 @@ document.addEventListener("DOMContentLoaded", () => {
 
   //Muestra (incluyendo PI) cada boton numerico
   const numberInput = (e) => {
+    if (e.target.value === '.' && !displayBottom.innerText.includes(e.target.value)) {
+      displayBottom.innerText += e.target.value;
+    }
     let number = parseFloat(e.target.value);
     if (e.target.id === "pi") {
-      number = Math.PI.toFixed(8);
+      number = Math.PI.toFixed(5);
     }
-    displayBottom.innerText = number;
+    if (number || number == 0) {
+
+      if (displayBottom.innerHTML.length <= 5) {
+
+        if (displayBottom.innerText === '0') {
+          displayBottom.innerText = number;
+        }
+        else {
+          displayBottom.innerText += number;
+        }
+
+      }
+
+    }
   }
+
+  const percentage = (num1, num2) =>{
+    return (num1*100)/num2;
+  }
+
+  const fraction = (num1) =>{
+    return 1/num1;
+  }
+
+  const square = (num1) =>{
+    return Math.pow(num1,2);
+  }
+
+  const squareRoot = (num1) =>{
+    return Math.sqrt(num1);
+  }
+
+  const divide = (num1,num2) =>{
+    if (num2 === 0){
+      return 'ERROR';
+    }
+    else{
+      return num1/num2;
+    }
+  }
+
+  const multiply = (num1,num2) =>{
+    return num1*num2;
+  }
+
+  const subtract = (num1,num2) =>{
+    return num1-num2;
+  }
+
+  const add = (num1,num2) =>{
+    return num1+num2;
+  }
+
+  const toggleSign = (num1) =>{
+    return num1*-1;
+  }
+
+
   //Recorre cada input
   numbers.forEach(n => {
     n.addEventListener('click', numberInput);
